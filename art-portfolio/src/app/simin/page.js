039332b0af6cd569650f7abf833e97f89a6a6f93
negarlatifian <negar.latifian@gmail.com>
2023-09-27 22:35:31 +0200

    added carousel to simin

--- art-portfolio/src/app/simin/page.js
+++ art-portfolio/src/app/simin/page.js
@@ -1,21 +1,23 @@
 'use client';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { nunito, nanum } from '../fonts';
 import Image from 'next/image';
-import { CldImage } from 'next-cloudinary';
-import './page.css';
 import { imageArray } from './links.js';
+import './page.css';
 
 const simin = () => {
   return (
     <section className='book'>
-      <h1 className={nunito.className}>O-AB-B-A-B-O-A-AB</h1>
+      <h1 className={nunito.className}>Simin</h1>
       <p className={nanum.className}>
         Simin is a project that explores the identity and expression of Iranian
         women who wear the mandatory hijab. The name Simin means "silvern" in
         Persian, and it reflects the two main aspects of the project: the use of
         silver as a material and a color, and the choice of Simin as a female
         name.
-        <br />
+      </p>
+      <p className={nanum.className}>
         The project consists of a series of portraits of young Iranian women in
         their modern outdoor outfits, contrasted with the imposed veil. The
         portraits are made using a technique inspired by the Daguerreotype
@@ -25,7 +27,8 @@ const simin = () => {
         unique and striking effect, where the viewer can see themselves
         reflected in the portrait, as well as the details of the subject's face
         and clothing.
-        <br />
+      </p>
+      <p className={nanum.className}>
         The project aims to challenge the stereotypes and prejudices that
         surround Iranian women, and to show their diversity and individuality.
         By using an old medium to capture the present, the project also creates
@@ -34,7 +37,20 @@ const simin = () => {
         of Iranian women, and their ability to express themselves despite the
         limitations.
       </p>
-      <div className='gallery'></div>
+      <div className='gallery'>
+        <Carousel showArrows={true} showThumbs={true} showIndicators={true}>
+          {imageArray.map((item) => (
+            <div key={item.title}>
+              <Image
+                src={item.imageSrc}
+                alt={item.title}
+                width={200}
+                height={500}
+              />
+            </div>
+          ))}
+        </Carousel>
+      </div>
     </section>
   );
 };
