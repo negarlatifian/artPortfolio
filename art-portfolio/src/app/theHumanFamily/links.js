f94231dab279cfae74878d132f5fe66b17a2710b
negarlatifian <negar.latifian@gmail.com>
2023-09-28 14:22:20 +0200

    theHumanFamily finished

---
+++ art-portfolio/src/app/theHumanFamily/links.js
@@ -0,0 +1,47 @@
+export const imageArray = [
+  {
+    id: 1,
+    title: 'Image 1',
+    width: 600,
+    height: 800,
+    className: 'big',
+    imageSrc:
+      'https://res.cloudinary.com/dh2zk8qgr/image/upload/v1695717884/theHumanFamily/01-s_szsyfo.png',
+  },
+  {
+    id: 2,
+    title: 'Image 2',
+    width: 360,
+    height: 480,
+    className: 'small',
+    imageSrc:
+      'https://res.cloudinary.com/dh2zk8qgr/image/upload/v1695717902/theHumanFamily/02-s_i1kjio.png',
+  },
+  {
+    id: 3,
+    title: 'Image 3',
+    width: 600,
+    height: 800,
+    className: 'big',
+    imageSrc:
+      'https://res.cloudinary.com/dh2zk8qgr/image/upload/v1695717903/theHumanFamily/03-s_zbc4qa.png',
+  },
+  {
+    id: 4,
+    title: 'Image 4',
+    width: 360,
+    height: 360,
+    className: 'small',
+    imageSrc:
+      'https://res.cloudinary.com/dh2zk8qgr/image/upload/v1695717909/theHumanFamily/05-s_y23me8.png',
+  },
+  {
+    id: 5,
+    title: 'Image 5',
+    width: 600,
+    height: 800,
+    className: 'big',
+    imageSrc:
+      'https://res.cloudinary.com/dh2zk8qgr/image/upload/v1695717904/theHumanFamily/04-s_hzpqeg.png',
+  },
+];
--- art-portfolio/src/app/theHumanFamily/page.js
+++ art-portfolio/src/app/theHumanFamily/page.js
@@ -1,7 +1,81 @@
-import React from 'react';
+'use client';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Lightbox from 'yet-another-react-lightbox';
+import 'yet-another-react-lightbox/styles.css';
+import { useState } from 'react';
+import { nunito, nanum } from '../fonts';
+import Image from 'next/image';
+import { imageArray } from './links.js';
+import './page.css';
 
-const collage = () => {
-  return <div>collage</div>;
+import Fullscreen from 'yet-another-react-lightbox/plugins/fullscreen';
+import Slideshow from 'yet-another-react-lightbox/plugins/slideshow';
+import Thumbnails from 'yet-another-react-lightbox/plugins/thumbnails';
+import Zoom from 'yet-another-react-lightbox/plugins/zoom';
+import 'yet-another-react-lightbox/plugins/thumbnails.css';
+
+const theHumanFamily = () => {
+  const [index, setIndex] = useState(-1);
+  return (
+    <section className='book'>
+      <h1 className={nunito.className}>
+        La Famille Humaine - The Human Family
+      </h1>
+      <p className={nanum.className}>
+        The human family is a collage collection that celebrates the diversity
+        and unity of humanity through the posters from the walls of Paris. The
+        posters from the walls of Paris are a rich source of images and texts
+        that reflect the history, culture, and politics of the city and its
+        people. They also show how the city is constantly changing and evolving,
+        just like the human family. I collected the posters from different
+        locations and periods in Paris, and then cut, tore, layered, and glued
+        them to create a collage for each individual. I used different colors
+        and shapes to create contrast and harmony between the layers, and to
+        highlight the unique characteristics and stories of each person. I also
+        used text fragments from the posters to create titles or captions for
+        each collage, adding another layer of meaning and context. My work is a
+        tribute to the human family and its diversity and unity. I believe that
+        art can be a way of connecting with others and learning from their
+        perspectives and experiences. I hope that my collages inspire others to
+        appreciate the beauty and complexity of humanity and its cultures. I
+        also want to continue exploring the possibilities and challenges of
+        collage as an artistic medium and expression.
+      </p>
+      <div className='gallery-container'>
+        <div
+          className='gallery'
+          onClick={({ index }) => {
+            console.log(index);
+            setIndex(1);
+          }}
+        >
+          {imageArray.map((item) => (
+            <div
+              className={[`${item.className}`, 'gallery-item']}
+              key={item.title}
+            >
+              <Image
+                src={item.imageSrc}
+                alt={item.title}
+                width={item.width}
+                height={item.height}
+              />
+            </div>
+          ))}
+        </div>
+        <Lightbox
+          slides={imageArray.map((item) => ({
+            src: item.imageSrc,
+            download: false,
+          }))}
+          open={index >= 0}
+          index={index}
+          close={() => setIndex(-1)}
+          plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+        />
+      </div>
+    </section>
+  );
 };
 
-export default collage;
+export default theHumanFamily;
