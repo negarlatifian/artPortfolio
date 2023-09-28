299489cc902abc34db00d3634e396b0bf95e876c
negarlatifian <negar.latifian@gmail.com>
2023-09-28 12:54:30 +0200

    carre page completed

---
+++ art-portfolio/src/app/carreDeFraternite/links.js
@@ -0,0 +1,74 @@
+export const imageArray = [
+  {
+    id: 1,
+    title: 'Image 1',
+    imageSrc:
+      'https://res.cloudinary.com/dh2zk8qgr/image/upload/v1695717455/carreDeFraternite/01_cjwkzy.jpg',
+  },
+  {
+    id: 2,
+    title: 'Image 2',
+    imageSrc:
+      'https://res.cloudinary.com/dh2zk8qgr/image/upload/v1695717482/carreDeFraternite/02_we198k.jpg',
+  },
+  {
+    id: 3,
+    title: 'Image 3',
+    imageSrc:
+      'https://res.cloudinary.com/dh2zk8qgr/image/upload/v1695717457/carreDeFraternite/03_xathim.jpg',
+  },
+  {
+    id: 4,
+    title: 'Image 4',
+    imageSrc:
+      'https://res.cloudinary.com/dh2zk8qgr/image/upload/v1695717459/carreDeFraternite/04_xh7zvh.jpg',
+  },
+  {
+    id: 5,
+    title: 'Image 5',
+    imageSrc:
+      'https://res.cloudinary.com/dh2zk8qgr/image/upload/v1695717455/carreDeFraternite/05_ltvdse.jpg',
+  },
+  {
+    id: 6,
+    title: 'Image 6',
+    imageSrc:
+      'https://res.cloudinary.com/dh2zk8qgr/image/upload/v1695717469/carreDeFraternite/06_ovkesr.jpg',
+  },
+  {
+    id: 7,
+    title: 'Image 7',
+    imageSrc:
+      'https://res.cloudinary.com/dh2zk8qgr/image/upload/v1695717476/carreDeFraternite/07_jz8ocr.jpg',
+  },
+  {
+    id: 8,
+    title: 'Image 8',
+    imageSrc:
+      'https://res.cloudinary.com/dh2zk8qgr/image/upload/v1695717468/carreDeFraternite/08_h157ae.jpg',
+  },
+  {
+    id: 9,
+    title: 'Image 9',
+    imageSrc:
+      'https://res.cloudinary.com/dh2zk8qgr/image/upload/v1695717466/carreDeFraternite/09_cpyhon.jpg',
+  },
+  {
+    id: 10,
+    title: 'Image 10',
+    imageSrc:
+      'https://res.cloudinary.com/dh2zk8qgr/image/upload/v1695717465/carreDeFraternite/10_jjlbnr.jpg',
+  },
+  {
+    id: 11,
+    title: 'Image 11',
+    imageSrc:
+      'https://res.cloudinary.com/dh2zk8qgr/image/upload/v1695717459/carreDeFraternite/11_vbdq0d.jpg',
+  },
+  {
+    id: 12,
+    title: 'Image 12',
+    imageSrc:
+      'https://res.cloudinary.com/dh2zk8qgr/image/upload/v1695717471/carreDeFraternite/12_krrqxw.jpg',
+  },
+];
--- art-portfolio/src/app/trappedInsideOutside/page.js
+++ art-portfolio/src/app/trappedInsideOutside/page.js
@@ -1,7 +1,78 @@
-import React from 'react';
+'use client';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Lightbox from 'yet-another-react-lightbox';
+import 'yet-another-react-lightbox/styles.css';
+import { useState } from 'react';
+import { nunito, nanum } from '../fonts';
+import Image from 'next/image';
+import { imageArray } from './links.js';
+import './page.css';
 
-const vitrin = () => {
-  return <div>vitrin</div>;
+import Fullscreen from 'yet-another-react-lightbox/plugins/fullscreen';
+import Slideshow from 'yet-another-react-lightbox/plugins/slideshow';
+import Thumbnails from 'yet-another-react-lightbox/plugins/thumbnails';
+import Zoom from 'yet-another-react-lightbox/plugins/zoom';
+import 'yet-another-react-lightbox/plugins/thumbnails.css';
+
+const carreDeFraternite = () => {
+  const [index, setIndex] = useState(-1);
+  return (
+    <section className='book'>
+      <h1 className={nunito.className}>
+        Carré de Fraternité - the Corner of Brotherhood
+      </h1>
+      <p className={nanum.className}>
+        Mattresses, old cardboard, blankets, sleeping bags, tent. A repetitive
+        scene in your daily walk in the streets of Paris. Each of these spots is
+        the territory of a homeless who is sleeping in the streets due to lots
+        of reasons.
+      </p>
+      <p className={nanum.className}>
+        There are 7-8,000 rough sleepers on the streets of Paris. There are some
+        shelters considered for homeless people but lack of capacity, illegal
+        immigration and strict rules of shelters force many of rough sleepers to
+        sleep in the street; Strict rules such as no dogs, no couples, no
+        alcohol and some also refuse to take those with mental issues.
+      </p>
+      <p className={nanum.className}>
+        Unfortunately, an average of 480 homeless people are reported to have
+        died in the streets of France every year, organizations like the
+        Collectif Les Morts organize simple ceremonies in a dedicated corner of
+        the Thiais Cemetery outside Paris. The area set aside for homeless
+        burials is called the Carré de Fraternité - the Corner of Brotherhood.
+      </p>
+      <div className='gallery-container'>
+        <div
+          className='gallery'
+          onClick={({ index }) => {
+            console.log(index);
+            setIndex(1);
+          }}
+        >
+          {imageArray.map((item) => (
+            <div className='gallery-item' key={item.title}>
+              <Image
+                src={item.imageSrc}
+                alt={item.title}
+                width={400}
+                height={300}
+              />
+            </div>
+          ))}
+        </div>
+        <Lightbox
+          slides={imageArray.map((item) => ({
+            src: item.imageSrc,
+            download: false,
+          }))}
+          open={index >= 0}
+          index={index}
+          close={() => setIndex(-1)}
+          plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+        />
+      </div>
+    </section>
+  );
 };
 
-export default vitrin;
+export default carreDeFraternite;
